bfde9157b38b10c083708529c7fdd021559fea77
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-04 17:51:43 -0500

    Exercise 3 on inheritance - method overloading

---
+++ src/inheritance/vehicle/exercises/Ex3.java
@@ -0,0 +1,33 @@
+package inheritance.vehicle.exercises;
+
+import inheritance.vehicle.Bus;
+
+public class Ex3 {
+
+    /**
+     *
+     * 		A final method in a SuperClass cannot be OVERRIDDEN
+     * 		by the child class.
+     *
+     * 		Given that the  setPrice() method
+     * 		in the parent class is final
+     *
+     * 		The overloaded method is not the same as the
+     * 		parent final setPrice() method
+     *
+     * 		setPrice(price);
+     *
+     * 		We don't need the keyword super.setPrice(price);
+     * 		because we have never overwritten it.
+     *
+     * */
+
+    public static void main(String[] args) {
+
+        Bus bus1 = new Bus();
+
+        // Using the subClass overridden method
+        bus1.setNumberOfDoors();;
+        System.out.println(bus1);
+    }
+}
