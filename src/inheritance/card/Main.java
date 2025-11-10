90d82225a775b9b5ebc830e15ca5a7603807df98
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-10 13:04:37 -0500

    Review of Card parent class and BirthDay subclass

--- src/inheritance/card/Main.java
+++ src/inheritance/card/Main.java
@@ -9,15 +9,18 @@ public class Main {
         Card card;
         BirthdayCard birthdayCard;
 
+        // Q1 : initializing a regular card
         card = new Card("Regular Card", "Enter your wishes");
         System.out.println(card);
         System.out.println();
 
+        // Q3 : initializing a birthday card and passing a birthday date
         LocalDate myBirthday = LocalDate.of(1990, 5, 15);
         birthdayCard = new BirthdayCard("Birthday", "Happy b-day, all the best!!", myBirthday);
         System.out.println(birthdayCard);
         System.out.println();
 
+        // Q3 : initializing a birthday card without birthday - setting birthday through setter
         birthdayCard = new BirthdayCard("Birthday", "Happy happy bdayyy");
         System.out.println(birthdayCard);
         birthdayCard.setBirthday(LocalDate.of(1988, 8, 27));
