092574ae14040a8ec11ac94190bcdb7c70ea02d4
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-11 09:18:24 -0500

    Refactoring the Main class from Rectangle for more clarity, adjusting the code and comments.

--- src/overloading/Main.java
+++ src/overloading/Main.java
@@ -5,24 +5,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Rectangle rectangleOne, rectangleTwo;
+        Rectangle rectangleOne;
         int area;
 
         rectangleOne = new Rectangle(10, 10);
         area = getArea(rectangleOne);
-        System.out.println("Area of rectangle " + area);
+        System.out.println("Area of a rectangle given a rectangle object: " + area);
 
-        rectangleTwo = new Rectangle(5, 5);
-        area = getArea(rectangleOne, rectangleTwo);
-        System.out.println("Area of two rectangles " + area);
+        area = getArea(10, 10);
+        System.out.println("Area of a given its length and width: " + area);
 
     }
 
     public static int getArea(Rectangle rectangle) {
-        return rectangle.getLenght() * rectangle.getWidth();
+        return getArea(rectangle.getLenght(), rectangle.getWidth());
     }
 
-    public static int getArea(Rectangle rectangleOne, Rectangle rectangleTwo) {
-        return (rectangleOne.getLenght() * rectangleOne.getWidth() + rectangleTwo.getLenght() * rectangleTwo.getWidth());
+    public static int getArea(int length, int width) {
+        return length * width;
     }
 }
