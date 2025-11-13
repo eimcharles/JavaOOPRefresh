bcc3ad517235d08e1bad1d506a88cdba086f9814
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-12 21:47:07 -0500

    Fixing comments in House class in example1

--- src/composition/example1/House.java
+++ src/composition/example1/House.java
@@ -1,18 +1,19 @@
 package composition.example1;
 
-/**
- *      Represents a House object that contains an Animal object.
- *
- *      This class demonstrates composition: a House "has a" Animal.
- *      The Animal is a part of the House.
- *
- *      The House does not control the creation or destruction of the Animal,
- *      the Animal object is associated to the House object.
- *
- *      A House may or may not have an associated Animal.
- */
-
 public class House {
+
+    /**
+     *      The House class represents a House object
+     *      that contains an Animal object.
+     *
+     *      This class demonstrates composition: a House "has a" Animal.
+     *      The Animal is a part of the House.
+     *
+     *      The House does not control the creation or destruction of the Animal,
+     *      the Animal object is associated to the House object.
+     *
+     *      A House may or may not have an associated Animal.
+     */
 
     private final String address;
     private Animal animal;
