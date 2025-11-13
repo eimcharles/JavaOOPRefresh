7e8760ce193a0ae5769222682bb6f632bbb8dddf
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-12 21:45:14 -0500

    Fixing comments in Main class in Shape package - redundant

--- src/abstraction/shape/Main.java
+++ src/abstraction/shape/Main.java
@@ -5,22 +5,6 @@ import abstraction.shape.subClass.Shape;
 
 public class Main {
 
-    /**
-     *     Encapsulation / Access Modifiers:
-     *       - `protected String color` allows subclasses
-     *       to access it while hiding it from unrelated classes.
-     *
-     *       - `protected abstract area()` enforces implementation
-     *       in subclasses without exposing it publicly.
-     *
-     *       - Public methods like `printArea()` and `displayColor()`
-     *       provide controlled access to external code.
-
-     *     Concrete Method Using Abstract Method:
-     *       `printArea()` calls the abstract `area()` method,
-     *       providing a public interface to the abstract calculation.
-     */
-
     public static void main(String[] args) {
 
         Shape shape = new Circle("Yellow", 5);
